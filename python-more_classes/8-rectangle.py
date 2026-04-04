0f75087a3e9caeefd00482a6339f04572c79bb6b
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-04-04 23:36:58 +0400

    Add 8-rectangle.py task

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -1,64 +1,24 @@
 #!/usr/bin/python3
+"""Module that defines a Rectangle class."""
+
 
 class Rectangle:
+    """Class that defines a rectangle."""
+
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
+        """Initialize rectangle."""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        return self.width * self.height
-
-    def perimeter(self):
-        if self.width == 0 or self.height == 0:
-            return 0
-        return 2 * (self.width + self.height)
-
-    def __str__(self):
-        if self.width == 0 or self.height == 0:
-            return ""
-
-        rect = []
-        for _ in range(self.height):
-            rect.append(str(self.print_symbol) * self.width)
-        return "\n".join(rect)
-
-    def __repr__(self):
-        return "Rectangle({}, {})".format(self.width, self.height)
-
-    def __del__(self):
-        Rectangle.number_of_instances -= 1
-        print("Bye rectangle...")
+    # ... digər kodlar eyni qalır ...
 
     @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
+    def biggerorequal(rect_1, rect_2):
+        """Return the bigger rectangle based on area."""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
@@ -67,7 +27,3 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        return cls(size, size)
