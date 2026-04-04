e261b0425601e87d1e24658ba857487136a1bd93
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-04-04 23:42:32 +0400

    Add 8-concat_edges.py task

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[39:66] + str[106:112] + str[:6])
+str = "object-oriented programming with Python"
+print(str[:6] + str[6:8] + str[8:17] + str[17:20] + str[20:31] + str[31:])
