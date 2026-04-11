f338575106526543b3f82bbf6fe8db28f0c9667c
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-04-11 23:18:36 +0400

    Add task

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Module that defines Fish, Bird, and FlyingFish classes.
+"""
+
+
+class Fish:
+    """
+    Fish class.
+    """
+
+    def swim(self):
+        """
+        Print swimming behavior.
+        """
+        print("The fish is swimming")
+
+    def habitat(self):
+        """
+        Print habitat of fish.
+        """
+        print("The fish lives in water")
+
+
+class Bird:
+    """
+    Bird class.
+    """
+
+    def fly(self):
+        """
+        Print flying behavior.
+        """
+        print("The bird is flying")
+
+    def habitat(self):
+        """
+        Print habitat of bird.
+        """
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """
+    FlyingFish class inheriting from Fish and Bird.
+    """
+
+    def swim(self):
+        """
+        Override swim method.
+        """
+        print("The flying fish is swimming!")
+
+    def fly(self):
+        """
+        Override fly method.
+        """
+        print("The flying fish is soaring!")
+
+    def habitat(self):
+        """
+        Override habitat method.
+        """
+        print("The flying fish lives both in water and the sky!")
