76b5128567f1e963266be74c03ea53c2ef8af2d6
Samir Mammadov <161446409+SamirMagb111111@users.noreply.github.com>
2026-04-11 22:23:14 +0400

    Add 6-base_geometry.py task

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that defines a BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class with an unimplemented area method.
+    """
+
+    def area(self):
+        """
+        Raises an exception indicating area is not implemented.
+        """
+        raise Exception("area() is not implemented")
